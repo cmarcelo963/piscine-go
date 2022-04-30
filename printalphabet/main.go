919099f63c1ccaf734f2a62159fb117a5d79de45
Administrator <administrator@Administrators-MacBook-Pro.local>
2022-04-30 15:36:53 +0100

    Deleted empty lines

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,12 +3,9 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-
 	var alphabet = []rune("abcdefghijklmnopqrstuvwxyz")
-
 	for i := 0; i < 26; i++ {
 		z01.PrintRune(alphabet[i])
 	}
-	z01.PrintRune('\n')
 	z01.PrintRune('\n')
 }
