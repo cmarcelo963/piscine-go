523fb689c64efdd652d09b7abb0bbe167b47054c
Administrator <administrator@Administrators-MacBook-Pro.local>
2022-05-02 20:24:53 +0100

    Print Comb 1.3

--- printcomb.go
+++ printcomb.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	for a := '0'; a <= '7'; a++ {
 		for b := 1 + a; b <= '8'; b++ {
-			for c := 1 + b; c <= '9'; c++{
+			for c := 1 + b; c <= '9'; c++ {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
